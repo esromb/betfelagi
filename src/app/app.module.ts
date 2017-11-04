75f637725adfd8b9deaed8990c12c4b45ec94c96
Esrom Waktola <eso@Esroms-MacBook-Pro.local>
2017-11-03 22:39:10 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/home/app.component';
+import { NavComponent } from './components/shared/app-nav/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
